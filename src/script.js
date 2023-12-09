db5d72d73eb939f1243fd9f6ec984876e74385f1
Ameer <ameerapex496@gmail.com>
2023-12-09 13:08:01 +0100

    added comment  / selected the elements needed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-console.log("hallo world");
+// selecting our elements
+var numberWrap = document.getElementsByClassName("number-Wrap")[0];
+var numberText = document.getElementsByClassName("number-Text")[0];
+var replayBtn = document.getElementsByClassName("replay-Btn")[0];
+// functions
+// event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,8 @@
-console.log("hallo world");
+// selecting our elements
+const numberWrap = document.getElementsByClassName("number-Wrap")[0];
+const numberText = document.getElementsByClassName("number-Text")[0];
+const replayBtn = document.getElementsByClassName("replay-Btn")[0];
+
+// functions
+
+// event lisnters
