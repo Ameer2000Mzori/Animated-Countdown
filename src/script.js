8ce8fbbe3b45c30bf8ea7c26c43e408ef85488bd
Ameer <ameerapex496@gmail.com>
2023-12-09 13:25:14 +0100

    added some styling changes / html changes / added function

--- src/script.js
+++ src/script.js
@@ -3,4 +3,46 @@ var numberWrap = document.getElementsByClassName("number-Wrap")[0];
 var numberText = document.getElementsByClassName("number-Text")[0];
 var replayBtn = document.getElementsByClassName("replay-Btn")[0];
 // functions
+var activateCountDown = function () {
+    replayBtn.style.display = "none";
+    setTimeout(function () {
+        numberText.textContent = "3";
+        numberText.style.color = "white";
+        numberWrap.classList.add("active");
+    }, 900);
+    setTimeout(function () {
+        numberWrap.classList.remove("active");
+        numberText.textContent = "";
+    }, 1900);
+    setTimeout(function () {
+        numberText.textContent = "2";
+        numberText.style.color = "yellow";
+        numberWrap.classList.add("active");
+    }, 2900);
+    setTimeout(function () {
+        numberWrap.classList.remove("active");
+        numberText.textContent = "";
+    }, 3900);
+    setTimeout(function () {
+        numberText.textContent = "1";
+        numberText.style.color = "blue";
+        numberWrap.classList.add("active");
+    }, 4900);
+    setTimeout(function () {
+        numberWrap.classList.remove("active");
+        numberText.textContent = "";
+    }, 5900);
+    setTimeout(function () {
+        numberText.textContent = "GO";
+        numberText.style.color = "green";
+        numberWrap.classList.add("active");
+    }, 6900);
+    setTimeout(function () {
+        numberText.textContent = "";
+        numberText.style.color = "";
+        numberWrap.classList.remove("active");
+        replayBtn.style.display = "block";
+    }, 7900);
+};
 // event lisnters
+replayBtn.addEventListener("click", activateCountDown);
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,57 @@
 // selecting our elements
-const numberWrap = document.getElementsByClassName("number-Wrap")[0];
-const numberText = document.getElementsByClassName("number-Text")[0];
-const replayBtn = document.getElementsByClassName("replay-Btn")[0];
+const numberWrap: any = document.getElementsByClassName("number-Wrap")[0];
+const numberText: any = document.getElementsByClassName("number-Text")[0];
+const replayBtn: any = document.getElementsByClassName("replay-Btn")[0];
 
 // functions
+const activateCountDown = (): void => {
+  replayBtn.style.display = "none";
+  setTimeout(() => {
+    numberText.textContent = "3";
+    numberText.style.color = `white`;
+    numberWrap.classList.add("active");
+  }, 900);
+
+  setTimeout(() => {
+    numberWrap.classList.remove("active");
+    numberText.textContent = "";
+  }, 1900);
+
+  setTimeout(() => {
+    numberText.textContent = "2";
+    numberText.style.color = `yellow`;
+    numberWrap.classList.add("active");
+  }, 2900);
+
+  setTimeout(() => {
+    numberWrap.classList.remove("active");
+    numberText.textContent = "";
+  }, 3900);
+
+  setTimeout(() => {
+    numberText.textContent = "1";
+    numberText.style.color = `blue`;
+    numberWrap.classList.add("active");
+  }, 4900);
+
+  setTimeout(() => {
+    numberWrap.classList.remove("active");
+    numberText.textContent = "";
+  }, 5900);
+
+  setTimeout(() => {
+    numberText.textContent = "GO";
+    numberText.style.color = `green`;
+    numberWrap.classList.add("active");
+  }, 6900);
+
+  setTimeout(() => {
+    numberText.textContent = "";
+    numberText.style.color = ``;
+    numberWrap.classList.remove("active");
+    replayBtn.style.display = "block";
+  }, 7900);
+};
 
 // event lisnters
+replayBtn.addEventListener("click", activateCountDown);
